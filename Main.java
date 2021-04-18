ecd9999f8fe90a5b463c990ecd67c52dad032b36
rmadduku <72843809+rmadduku@users.noreply.github.com>
2021-04-17 19:28:22 -0700

    added comments

--- Main.java
+++ Main.java
@@ -5,6 +5,10 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 
+
+//Runtime is (10)! * (M*N)
+//Where M is the length of the first character list
+// where N is the length of the second character list
 //Given a large number of binary files, write a program that finds the
 //longest strand of bytes that is identical between two or more files
 //
@@ -56,72 +60,121 @@ public class Main {
 		FileInfo File9 = new FileInfo(inString9);
 		FileInfo File10 = new FileInfo(inString10);
 		
+		// contains
+		// the length of the largest strand in index [0]
+		// the offset of largest strand in index [1]
+		// the offset of largest strand in the other file in a[2]
 		int[] winners = new int[3];
 
 		
 		
 		//comparing 1 against everyone
-		winners = compare(length(File1.list, File2.list), winners,File1.getName(),File2.getName());
-		winners = compare(length(File1.list, File3.list), winners,File1.getName(),File3.getName());
-		winners = compare(length(File1.list, File4.list), winners, File1.getName(),File4.getName());
-		winners = compare(length(File1.list, File5.list), winners, File1.getName(),File5.getName());
-		winners = compare(length(File1.list, File6.list), winners,File1.getName(),File6.getName());
-		winners = compare(length(File1.list, File7.list), winners,File1.getName(),File7.getName());
-		winners = compare(length(File1.list, File8.list), winners,File1.getName(),File8.getName());
-		winners = compare(length(File1.list, File9.list), winners,File1.getName(),File9.getName());
-		winners = compare(length(File1.list, File10.list), winners,File1.getName(),File10.getName());
+		winners = 
+			compare(length(File1.list, File2.list), winners,File1.getName(),File2.getName());
+		winners = 
+			compare(length(File1.list, File3.list), winners,File1.getName(),File3.getName());
+		winners = 
+			compare(length(File1.list, File4.list), winners, File1.getName(),File4.getName());
+		winners = 
+			compare(length(File1.list, File5.list), winners, File1.getName(),File5.getName());
+		winners = 
+			compare(length(File1.list, File6.list), winners,File1.getName(),File6.getName());
+		winners = 
+			compare(length(File1.list, File7.list), winners,File1.getName(),File7.getName());
+		winners = 
+			compare(length(File1.list, File8.list), winners,File1.getName(),File8.getName());
+		winners = 
+			compare(length(File1.list, File9.list), winners,File1.getName(),File9.getName());
+		winners = 
+			compare(length(File1.list, File10.list), winners,File1.getName(),File10.getName());
 		
 		//comparing 2 against everyone
-		winners = compare(length(File2.list, File3.list), winners, File2.getName(),File3.getName());
-		winners = compare(length(File2.list, File4.list), winners, File2.getName(),File4.getName());
-		winners = compare(length(File2.list, File5.list), winners, File2.getName(),File5.getName());
-		winners = compare(length(File2.list, File6.list), winners, File2.getName(),File6.getName());
-		winners = compare(length(File2.list, File7.list), winners, File2.getName(),File7.getName());
-		winners = compare(length(File2.list, File8.list), winners, File2.getName(),File8.getName());
-		winners = compare(length(File2.list, File9.list), winners, File2.getName(),File9.getName());
-		winners = compare(length(File2.list, File10.list), winners,File2.getName(),File10.getName());
+		winners = 
+			compare(length(File2.list, File3.list), winners, File2.getName(),File3.getName());
+		winners = 
+			compare(length(File2.list, File4.list), winners, File2.getName(),File4.getName());
+		winners = 
+			compare(length(File2.list, File5.list), winners, File2.getName(),File5.getName());
+		winners = 
+			compare(length(File2.list, File6.list), winners, File2.getName(),File6.getName());
+		winners = 
+			compare(length(File2.list, File7.list), winners, File2.getName(),File7.getName());
+		winners = 
+			compare(length(File2.list, File8.list), winners, File2.getName(),File8.getName());
+		winners = 
+			compare(length(File2.list, File9.list), winners, File2.getName(),File9.getName());
+		winners = 
+			compare(length(File2.list, File10.list), winners,File2.getName(),File10.getName());
 		
 		//comparing 3 against everyone
-		winners = compare(length(File3.list, File4.list), winners, File3.getName(),File4.getName());
-		winners = compare(length(File3.list, File5.list), winners, File3.getName(),File5.getName());
-		winners = compare(length(File3.list, File6.list), winners,File3.getName(),File6.getName());
-		winners = compare(length(File3.list, File7.list), winners,File3.getName(),File7.getName());
-		winners = compare(length(File3.list, File8.list), winners,File3.getName(),File8.getName());
-		winners = compare(length(File3.list, File9.list), winners,File3.getName(),File9.getName());
-		winners = compare(length(File3.list, File10.list), winners,File3.getName(),File10.getName());
+		winners = 
+			compare(length(File3.list, File4.list), winners, File3.getName(),File4.getName());
+		winners = 
+			compare(length(File3.list, File5.list), winners, File3.getName(),File5.getName());
+		winners = 
+			compare(length(File3.list, File6.list), winners,File3.getName(),File6.getName());
+		winners = 
+			compare(length(File3.list, File7.list), winners,File3.getName(),File7.getName());
+		winners = 
+			compare(length(File3.list, File8.list), winners,File3.getName(),File8.getName());
+		winners = 
+			compare(length(File3.list, File9.list), winners,File3.getName(),File9.getName());
+		winners = 
+			compare(length(File3.list, File10.list), winners,File3.getName(),File10.getName());
 		
 		//comparing 4 against everyone
-		winners = compare(length(File4.list, File5.list), winners, File4.getName(),File5.getName());
-		winners = compare(length(File4.list, File6.list), winners,File4.getName(),File6.getName());
-		winners = compare(length(File4.list, File7.list), winners,File4.getName(),File7.getName());
-		winners = compare(length(File4.list, File8.list), winners,File4.getName(),File8.getName());
-		winners = compare(length(File4.list, File9.list), winners,File4.getName(),File9.getName());
-		winners = compare(length(File4.list, File10.list), winners,File4.getName(),File10.getName());
+		winners = 
+			compare(length(File4.list, File5.list), winners, File4.getName(),File5.getName());
+		winners = 
+			compare(length(File4.list, File6.list), winners,File4.getName(),File6.getName());
+		winners = 
+			compare(length(File4.list, File7.list), winners,File4.getName(),File7.getName());
+		winners =
+			compare(length(File4.list, File8.list), winners,File4.getName(),File8.getName());
+		winners =
+			compare(length(File4.list, File9.list), winners,File4.getName(),File9.getName());
+		winners =
+			compare(length(File4.list, File10.list), winners,File4.getName(),File10.getName());
 		
 		//comparing 5 against everyone
-		winners = compare(length(File5.list, File6.list), winners,File5.getName(),File6.getName());
-		winners = compare(length(File5.list, File7.list), winners,File5.getName(),File7.getName());
-		winners = compare(length(File5.list, File8.list), winners,File5.getName(),File8.getName());
-		winners = compare(length(File5.list, File9.list), winners,File5.getName(),File9.getName());
-		winners = compare(length(File5.list, File10.list), winners,File5.getName(),File10.getName());
+		winners = 
+			compare(length(File5.list, File6.list), winners,File5.getName(),File6.getName());
+		winners = 
+			compare(length(File5.list, File7.list), winners,File5.getName(),File7.getName());
+		winners = 
+			compare(length(File5.list, File8.list), winners,File5.getName(),File8.getName());
+		winners = 
+			compare(length(File5.list, File9.list), winners,File5.getName(),File9.getName());
+		winners = 
+			compare(length(File5.list, File10.list), winners,File5.getName(),File10.getName());
 		
 		//comparing 6 against everyone
-		winners = compare(length(File6.list, File7.list), winners,File6.getName(),File7.getName());
-		winners = compare(length(File6.list, File8.list), winners,File6.getName(),File8.getName());
-		winners = compare(length(File6.list, File9.list), winners,File6.getName(),File9.getName());
-		winners = compare(length(File6.list, File10.list), winners,File6.getName(),File10.getName());
+		winners = 
+			compare(length(File6.list, File7.list), winners,File6.getName(),File7.getName());
+		winners =
+			compare(length(File6.list, File8.list), winners,File6.getName(),File8.getName());
+		winners = 
+			compare(length(File6.list, File9.list), winners,File6.getName(),File9.getName());
+		winners =
+			compare(length(File6.list, File10.list), winners,File6.getName(),File10.getName());
 		
 		//comparing 7 against everyone
-		winners = compare(length(File7.list, File8.list), winners,File7.getName(),File8.getName());
-		winners = compare(length(File7.list, File9.list), winners,File7.getName(),File9.getName());
-		winners = compare(length(File7.list, File10.list), winners,File7.getName(),File10.getName());
+		winners = 
+			compare(length(File7.list, File8.list), winners,File7.getName(),File8.getName());
+		winners = 
+			compare(length(File7.list, File9.list), winners,File7.getName(),File9.getName());
+		winners = 
+			compare(length(File7.list, File10.list), winners,File7.getName(),File10.getName());
 		
 		//comparing 8 against everyone
-		winners = compare(length(File8.list, File9.list), winners,File8.getName(),File9.getName());
-		winners = compare(length(File8.list, File10.list), winners,File8.getName(),File10.getName());
+		winners =
+			compare(length(File8.list, File9.list), winners,File8.getName(),File9.getName());
+		winners =
+			compare(length(File8.list, File10.list), winners,File8.getName(),File10.getName());
 		
 		//comparing 9 against everyone
-		winners = compare(length(File9.list, File10.list), winners,File9.getName(),File10.getName());
+		winners =
+			compare(length(File9.list, File10.list), winners,File9.getName(),File10.getName());
 		
 		
 		System.out.println("length of the largest strand:"+winners[0]);
@@ -132,7 +185,14 @@ public class Main {
 		
 	}
 	
-	
+	/**
+	* Takes in two Byte Lists and compares them to find the largest
+	* common strand, as well as the offset
+	* @param a first byte list to evalute
+	* @param b second byte list to evalute
+	* @return an integer array containing
+	* the length and offset of the largest strand
+	**/
 	public static int[] length(ArrayList<Byte> a,ArrayList<Byte> b) {
 		int table[][] = new int[2][b.size()+1];
 		int res =0;
@@ -144,6 +204,7 @@ public class Main {
             {
                 if(a.get(i-1)==b.get(j-1))
                 {
+		//keeps a running total of shared bytes
                     table[i%2][j]=table[(i-1)%2][j-1]+1;
                     if(table[i%2][j]>res) {
                     	offset1 =i;
@@ -157,11 +218,17 @@ public class Main {
         }
         return new int[]{res,offset1-res,offset2-res};
     }
-	
+	/**
+	* Function to compare the length of the strands, and save 
+	* the file where it occured, and the offset
+	* @param a first length, and offset trio to evaluate
+	* @param b second length, and offset trio to evaluate
+	* @param x name of file where largest strand in a occurs
+	* @param y name of file where largest strand in b occurs
+	* @return the length and offset trio of the larger strand
+	**/
 	public static int[] compare(int[] a,int[] b, String x, String y) {
 		if(a[0] == b[0]) {
-//			System.out.println("if:"+a[0]);
-//			System.out.println(b[0]);
 			if(!dupes.contains(x)) {
 				dupes.add(x);
 				dupes.add(a[1]+"");
@@ -174,8 +241,6 @@ public class Main {
 		}
 
 		if(a[0]>b[0]) {
-//			System.out.println("if else:"+a[0]);
-//			System.out.println(b[0]);
 			dupes = new ArrayList<>();
 			if(!dupes.contains(x)) {
 				dupes.add(x);
@@ -187,9 +252,6 @@ public class Main {
 			}
 			return a;
 		}
-//		System.out.println("else:"+a[0]);
-//		System.out.println(b[0]);
-
 		return b;
 	}
 }
@@ -197,7 +259,9 @@ public class Main {
 	
 	
 
-
+/**
+* Class to help parse and access each file easier
+**/
 class FileInfo{
 	ArrayList<Byte> list;
 	FileInputStream fileInputStream;
